4265aa5074f695ca9c1d834003de76609b108671
wutango <nickkdb@gmail.com>
2021-02-28 19:11:27 -0500

    fixed test params for engineers

--- test/Engineer.test.js
+++ test/Engineer.test.js
@@ -1,19 +1,26 @@
 const Engineer = require("../lib/Engineer");
 
+const data= {
+  name: "Foo",
+  id: 1,
+  email: "test@test.com",
+  github: "GitHubUser"
+}
+
 test("Can set GitHUb account via constructor", () => {
   const testValue = "GitHubUser";
-  const e = new Engineer("Foo", 1, "test@test.com", testValue);
+  const e = new Engineer(data);
   expect(e.github).toBe(testValue);
 });
 
 test("getRole() should return \"Engineer\"", () => {
   const testValue = "Engineer";
-  const e = new Engineer("Foo", 1, "test@test.com", "GitHubUser");
+  const e = new Engineer(data);
   expect(e.getRole()).toBe(testValue);
 });
 
 test("Can get GitHub username via getGithub()", () => {
   const testValue = "GitHubUser";
-  const e = new Engineer("Foo", 1, "test@test.com", testValue);
+  const e = new Engineer(data);
   expect(e.getGithub()).toBe(testValue);
 });
